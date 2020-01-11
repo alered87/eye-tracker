f71ddc742c401564ab14c99dae0de3f111230dd4
alered87 <alered87@gmail.com>
2020-01-11 08:16:00 +0100

    adding saccade extraction and training

--- extraction.py
+++ extraction.py
@@ -1,12 +1,18 @@
 import numpy as np
 import pandas as pd
+from scipy.stats import zscore
+import matplotlib.pyplot as plt
 
 from config import *
 
 
+def compute_outlier_threshold(pandas_series):
+    return (pandas_series.quantile(0.75) - pandas_series.quantile(0.25)) * 1.5 + pandas_series.quantile(0.75)
+
+
 def clean_fixations(raw_df, x_min=0, x_max=1024, x_name='x',
                     y_min=0, y_max=768, y_name='y',
-                    duration_name='duration', fix_min=100.0, fix_max=400.0):
+                    duration_name='duration', fix_min=100.0, fix_max=400.0, fix_cleaning='minmax'):
     """
         Clean noisy fixations data depending on x, y boundaries and fixations min-max duration
 
@@ -20,6 +26,8 @@ def clean_fixations(raw_df, x_min=0, x_max=1024, x_name='x',
     :param duration_name:
     :param fix_min:
     :param fix_max:
+    :param fix_cleaning:
+
     :return:
     """
     # Clean x position
@@ -29,19 +37,38 @@ def clean_fixations(raw_df, x_min=0, x_max=1024, x_name='x',
     raw_df = raw_df[(raw_df[y_name] > y_min) & (raw_df[y_name] < y_max)]
 
     # Clean Fixations
-    raw_df = raw_df[(raw_df[duration_name] > fix_min) & (raw_df[duration_name] < fix_max)]
+    if fix_cleaning == 'minmax':
+        raw_df = raw_df[(raw_df[duration_name] > fix_min) & (raw_df[duration_name] < fix_max)]
+
+    elif fix_cleaning == 'z_score':
+        raw_df = raw_df[np.abs(zscore(raw_df[duration_name])) < 3]  # Cleaning by removing outlier
+
+    elif fix_cleaning == 'outliers_iter':
+        last_thr = raw_df[duration_name].max()
+        c_fix_thr = compute_outlier_threshold(raw_df[duration_name])
+        while (last_thr - c_fix_thr) > 0.0:
+            raw_df = raw_df[raw_df[duration_name] < c_fix_thr]
+            print(last_thr, c_fix_thr)
+            last_thr = c_fix_thr
+            c_fix_thr = compute_outlier_threshold(raw_df[duration_name])
 
     return raw_df
 
 
-def extract_fixations_stats(df, compute_stats=True, compute_areas_stats=True,
+def extract_fixations_stats(df, compute_stats=True, compute_areas_stats=True, clean_areas=True,
                             session_name='session', image='image', area='area', int_area='int_area',
-                            start='start', trigger_duration=100.0, tail=5,
+                            start='start', trigger_duration=100.0, tail=5, minimum_fixation_length=100.0,
                             pupil_size_name='pupil', norm_pupil_size_name='norm_pupil_size',
+                            norm0_pupil_size_name='norm0_pupil_size', x='CURRENT_FIX_X', y='CURRENT_FIX_Y',
                             duration='duration', norm_duration='norm_duration'):
     """
     Clean fixations data-frame and compute statistics image-wise [optional] and/or area-wise [optional]
 
+    :param clean_areas:
+    :param y:
+    :param x:
+    :param minimum_fixation_length:
+    :param norm0_pupil_size_name:
     :param df:
     :param compute_stats:
     :param compute_areas_stats:
@@ -75,9 +102,18 @@ def extract_fixations_stats(df, compute_stats=True, compute_areas_stats=True,
         'pupil_mean',  # Average pupil size
         'pupil_std',  # Std of pupil size
         # Normalized Pupil Size
-        'norm_fix_max',  # Normalized max pupil size
-        'norm_fix_mean',  # Normalized average pupil size
-        'norm_fix_std'  # Normalized std of pupil size
+        'norm_pupil_max',  # Normalized max pupil size
+        'norm_pupil_mean',  # Normalized average pupil size
+        'norm_pupil_std',  # Normalized std of pupil size
+        # 1st pupil size Normalized Pupil Size
+        'norm0_pupil_max',  # Normalized max pupil size
+        'norm0_pupil_mean',  # Normalized average pupil size
+        'norm0_pupil_std',  # Normalized std of pupil size
+        # Total task time
+        'task_time',
+        'minimum_length',
+        'x_regressions',
+        'y_regressions'
     ]
     agg_data = []  # Data of all the statistics on cleaned images
     areas_agg_data = []  # Data of all the statistics on cleaned images, computed by area-of-interest
@@ -94,6 +130,7 @@ def extract_fixations_stats(df, compute_stats=True, compute_areas_stats=True,
 
         # Subject-wise Normalization
         cdf[norm_pupil_size_name] = cdf[pupil_size_name] - cdf[pupil_size_name].mean()
+        cdf[norm0_pupil_size_name] = cdf[pupil_size_name] - cdf[pupil_size_name].iloc[0]
         cdf[norm_duration] = cdf[duration] - cdf[duration].mean()
 
         for img in cdf[image].unique():
@@ -102,36 +139,41 @@ def extract_fixations_stats(df, compute_stats=True, compute_areas_stats=True,
             idf = cdf[cdf[image] == img]
 
             # # # Clean head
-            idf[int_area] = idf[area].apply(lambda x: AREAS_DICT[x])  # Convert areas names in int
-            idf['trigger'] = idf[int_area] == 99  # Trigger area detection
+            if trigger_duration > 0:
+                idf[int_area] = idf[area].apply(lambda x: AREAS_DICT[x])  # Convert areas names in int
+                idf['trigger'] = idf[int_area] == 99  # Trigger area detection
 
-            # Detect first fixation after trigger
-            start_ind = idf[[all([a, b]) for a, b in zip(idf['trigger'], idf[duration] > trigger_duration)]].index.min()
+                # Detect first fixation after trigger
+                start_ind = idf[[all([a, b]) for a, b in zip(idf['trigger'], idf[duration] > trigger_duration)]].index.min()
 
-            # Skipping too short trial
-            if len(idf.loc[start_ind:]) <= 1:
+                # Skipping too short trial
+                if len(idf.loc[start_ind:]) <= 1:
 
-                fix_start_dict[s][img] = -1
-                print(" >>>>> SKIPPING: {}{}".format(s, img))
-                if s in skipped.keys():
-                    skipped[s].append(img)
-                else:
-                    skipped[s] = [img]
-                continue
+                    fix_start_dict[s][img] = -1
+                    print(" >>>>> SKIPPING: {}{}".format(s, img))
+                    if s in skipped.keys():
+                        skipped[s].append(img)
+                    else:
+                        skipped[s] = [img]
+                    continue
 
-            start_loc = idf.index.get_loc(start_ind) + 1  # Compute initial index
+                start_loc = idf.index.get_loc(start_ind) + 1  # Compute initial index
 
-            # Saving start, end time (can be used to filter raw gaze data)
-            fix_start_dict[s][img] = (idf[start].iloc[start_loc], idf[start].iloc[-tail])
-            idf = idf.iloc[start_loc:]  # Selecting final data
+                # Saving start, end time (can be used to filter raw gaze data)
+                fix_start_dict[s][img] = (idf[start].iloc[start_loc], idf[start].iloc[-tail])
+                idf = idf.iloc[start_loc:]  # Selecting final data
 
-            # Clean Interest areas
-            idf = idf[idf[int_area].isin([
-                1, 5, 9, 13, 99,  # 1st Row
-                2, 6, 10, 14,  # 2nd Row
-                3, 7, 11, 15,  # 3nd Row
-                4, 8, 12, 16  # 4th Row
-            ])]
+            # Clean too short fixations
+            idf = idf[idf[duration] > minimum_fixation_length]
+
+            if clean_areas:
+                # Clean Interest areas
+                idf = idf[idf[int_area].isin([
+                    1, 5, 9, 13, 99,  # 1st Row
+                    2, 6, 10, 14,  # 2nd Row
+                    3, 7, 11, 15,  # 3nd Row
+                    4, 8, 12, 16  # 4th Row
+                ])]
 
             # Append data on global DataFrame
             if len(idf) == 0:
@@ -171,7 +213,20 @@ def extract_fixations_stats(df, compute_stats=True, compute_areas_stats=True,
                     # Normalized Pupil Size
                     idf[norm_pupil_size_name].max(),  # Normalized max pupil size
                     idf[norm_pupil_size_name].mean(),  # Normalized average pupil size
-                    idf[norm_pupil_size_name].std()  # Normalized std of pupil size
+                    idf[norm_pupil_size_name].std(),  # Normalized std of pupil size
+                    # 1st pupil size Normalized Pupil Size
+                    idf[norm0_pupil_size_name].max(),  # Normalized max pupil size
+                    idf[norm0_pupil_size_name].mean(),  # Normalized average pupil size
+                    idf[norm0_pupil_size_name].std(),  # Normalized std of pupil size
+
+                    # Total task
+                    idf.iloc[-1][duration] + idf.iloc[-1][start] - idf.iloc[0][start],
+
+                    minimum_fixation_length,
+
+                    np.sum([-300 < (idf[x].iloc[i] - idf[x].iloc[i - 1]) < 0 for i in range(1, len(idf))]),
+                    np.sum([(idf[y].iloc[i] - idf[y].iloc[i - 1]) < 0 for i in range(1, len(idf))])
+
                 ]
 
                 agg_data.append(new_data)
@@ -196,9 +251,105 @@ def extract_fixations_stats(df, compute_stats=True, compute_areas_stats=True,
     return_list = [clean_df, fix_start_dict, skipped]
 
     if compute_stats:
-        return_list.append(pd.DataFrame(data=agg_data, columns=new_columns))
+        clean_stats = pd.DataFrame(data=agg_data, columns=new_columns)
+        clean_stats[image].hist()
+        plt.show()
+        return_list.append(clean_stats)
 
     if compute_areas_stats:
         return_list.append(np.nan_to_num(areas_agg_data))
 
     return return_list
+
+
+def saccade_distance(row, x_e, x_s, y_e, y_s):
+    return np.sqrt((row[x_e]-row[x_s])**2 + (row[y_e]-row[y_s])**2)
+
+
+def saccade_slope(row, x_e, x_s, y_e, y_s):
+    return (row[y_e]-row[y_s])/(row[x_e]-row[x_s])
+
+
+def saccade_stats(saccades_df, image='image'):
+
+    new_columns = [
+        image,
+        # Saccade Direction counting
+        'up_freq', 'down_freq', 'left_freq', 'right_freq',
+        # Blink counting and stats
+        'n_blink', 'min_blink', 'avg_blink', 'max_blink',
+
+    ]
+    for c_var in ['duration', 'vel', 'ampl', 'angle', 'distance', 'slope']:
+        for c_measure in ['min', 'avg', 'max']:#, 'std']:
+            new_columns.append("{0}_{1}".format(c_measure, c_var))
+
+    x_start, y_start = 'CURRENT_SAC_START_X', 'CURRENT_SAC_START_Y'
+    x_end, y_end = 'CURRENT_SAC_END_X', 'CURRENT_SAC_END_Y'
+    angle = 'CURRENT_SAC_ANGLE'
+    ampl = 'CURRENT_SAC_AMPLITUDE'
+    vel = 'CURRENT_SAC_AVG_VELOCITY'
+    duration = 'CURRENT_SAC_DURATION'
+    blink_duration = 'CURRENT_SAC_BLINK_DURATION'
+    area = 'CURRENT_SAC_END_INTEREST_AREA_ID'
+    direction = 'CURRENT_SAC_DIRECTION'
+    distance = 'distance'
+    slope = 'slope'
+
+    float_vars = [x_start, y_start, x_end, y_end, angle, ampl, vel, duration]
+    int_vars = [area]
+    clean_vars = [direction]
+
+    gdf = saccades_df.copy()
+    gdf[blink_duration] = gdf[blink_duration].apply(lambda x: '0' if x == '.' else x).astype(int)
+
+    # Cleaning empty values
+    for c_var in clean_vars + float_vars + int_vars:
+        gdf = gdf[gdf[c_var] != '.']
+
+    for c_var in float_vars:
+        gdf[c_var] = gdf[c_var].astype(float)
+
+    for c_var in int_vars:
+        gdf[c_var] = gdf[c_var].astype(int)
+
+    gdf[distance] = gdf.apply(saccade_distance, args=(x_end, x_start, y_end, y_start), axis=1)
+    gdf[slope] = gdf.apply(saccade_slope, args=(x_end, x_start, y_end, y_start), axis=1)
+
+    all_data = []
+    for img in gdf[image].unique():
+
+        new_data = [IMAGES_DICT[img]]
+        # Selecting current image
+        df = gdf[gdf[image] == img]
+
+        # Saccades Directions counting
+        direction_count = df.groupby([direction])['experiment'].count()
+        for c_direction in ['UP', 'DOWN', 'LEFT', 'RIGHT']:
+            if c_direction in direction_count:
+                new_data.append(direction_count[c_direction]/direction_count.sum())
+            else:
+                new_data.append(0)
+
+        # Blink counting and duration
+        blink_df = df[df[blink_duration] != 0]
+        new_data.append(len(blink_df))
+
+        new_row = [0, 0, 0]
+        if len(blink_df) > 0:
+            new_row = [
+                blink_df[blink_duration].min(),
+                blink_df[blink_duration].mean(),
+                blink_df[blink_duration].max()
+            ]
+
+        new_data += new_row
+
+        for stat_var in [duration, vel, ampl, angle, distance, slope]:
+            new_data += [df[stat_var].min(), df[stat_var].mean(), df[stat_var].max()]#, df[stat_var].std()]
+
+        all_data.append(new_data)
+
+    return pd.DataFrame(data=all_data, columns=new_columns)
+
+
--- training.py
+++ training.py
@@ -1,27 +1,48 @@
+import sys
+
 import numpy as np
+import pandas as pd
 
 from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix, f1_score, accuracy_score
 from sklearn.model_selection import train_test_split, GridSearchCV
 
 
-def grid_search(data, labels, test_size=0.25, multi_class=False, normalizer_function=MinMaxScaler):
+def grid_search_notest(data, labels, multi_class=False, score='accuracy',
+                normalizer_function=MinMaxScaler, classifier=SVC, hyper_parameters=None):
 
-    # Set the parameters by cross-validation
-    gamma = [100, 10, 1, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5]
-    c_weight = [0.01, 0.1, 1, 5, 10, 20, 50, 100, 200, 500, 1000]
-    deg = [3, 6, 9]
-    class_weight = [{0: 1, 1: 1}, {0: 1, 1: 2}, {0: 2, 1: 1}, {0: 1, 1: 3}, {0: 3, 1: 1}, {0: 1, 1: 4}, {0: 4, 1: 1}]
+    if hyper_parameters is None:
+        hyper_parameters = {'kernel': ['rbf'], 'gamma': [1]}
 
-    hyper_parameters = [
-        {'kernel': ['rbf'], 'gamma': gamma, 'C': c_weight, 'class_weight': class_weight},
-        {'kernel': ['linear'], 'C': c_weight, 'class_weight': class_weight},
-        {'kernel': ['poly'], 'degree': deg, 'gamma': gamma, 'C': c_weight, 'class_weight': class_weight}
-    ]
     if multi_class:
         for el in hyper_parameters:
-            el['decision_function_shape'] =  ['ovo']
+            el['decision_function_shape'] = ['ovo']
+
+    normalizer = normalizer_function()
+    x_train = normalizer.fit_transform(data)
+
+    print("# Tuning hyper-parameters for %s" % score)
+    print()
+
+    clf = GridSearchCV(classifier(), hyper_parameters, cv=5, scoring=score, n_jobs=6, verbose=5)
+    clf.fit(x_train, labels)
+
+    return clf
+
+
+def grid_search(data, labels, test_size=0.25, multi_class=False,
+                normalizer_function=MinMaxScaler, classifier=SVC, hyper_parameters=None):
+
+    if hyper_parameters is None:
+        hyper_parameters = {'kernel': ['rbf'], 'gamma': [1]}
+
+    if multi_class:
+        for el in hyper_parameters:
+            el['decision_function_shape'] = ['ovo']
 
     # Split the data set in two equal parts
     x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=test_size, random_state=None)
@@ -35,7 +56,7 @@ def grid_search(data, labels, test_size=0.25, multi_class=False, normalizer_func
         print("# Tuning hyper-parameters for %s" % score)
         print()
 
-        clf = GridSearchCV(SVC(), hyper_parameters, cv=5, scoring=score, n_jobs=6, verbose=5)
+        clf = GridSearchCV(classifier(), hyper_parameters, cv=5, scoring=score, n_jobs=6, verbose=5)
         clf.fit(x_train, y_train)
 
         print("Best parameters set found on development set:")
@@ -73,3 +94,201 @@ def compute_confusion(classifier, data_list):
         print(confusion_matrix(data_list[i + 2], yp))
 
     return c_acc
+
+
+if __name__ == '__main__':
+
+    test_rate = float(sys.argv[1])
+    saccade = sys.argv[2] == 's'
+    sbj_normalize = sys.argv[3] == 'n'
+
+    out_name = 'results'
+
+    if saccade:
+        data_path = '/Users/alerossi/Downloads/jupyter_sketches/output_fixESac.csv'
+        out_name += '_saccade'
+    else:
+        data_path = '/Users/alerossi/Downloads/jupyter_sketches/output.csv'
+        out_name += '_norm'
+
+    # data_path = 'output.csv'
+    class_column = 'image'
+    """
+            SETTING TEST PARAMETERS
+    
+    """
+    trials = 10
+
+    scores = [
+        'accuracy',
+        'f1'
+    ]
+
+    out_cols = ['Test', 'Normalization', 'Classifier', 'Min_length', 'Accuracy', 'F1', 'parameters']
+
+    if test_rate == 0:
+        trials = 1
+        out_name += '_notest'
+        out_cols = ['Test', 'Normalization', 'Classifier', 'Min_length']
+        if 'accuracy' in scores:
+            out_cols += ['Accuracy_Avg', 'Accuracy_Std', 'Accuracy_parameters']
+        if 'f1' in scores:
+            out_cols += ['F1_Avg', 'F1_Std', 'F1_parameters']
+
+    out_name += '.csv'
+    normalizers = {
+        'minmax': MinMaxScaler,
+        'zmean': StandardScaler
+    }
+
+    classifier_to_test = {
+        'svm': SVC,
+        'log-reg': LogisticRegression,
+        'RF': RandomForestClassifier,
+        'NN': MLPClassifier
+    }
+
+    # Set the parameters by cross-validation
+    gamma = ['auto', 'scale']#[100,  10, 1, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5] # 8
+    c_weight = [0.001, 0.01, 0.1, 1, 10, 20] # 11
+    deg = [3, 6, 9]
+    class_weight = [{0: 1, 1: 1}, {0: 1, 1: 2}, {0: 2, 1: 1}]#, {0: 1, 1: 3}, {0: 3, 1: 1}, {0: 1, 1: 4}, {0: 4, 1: 1}] #7
+    lr = [0.01, 0.001]
+    hu = [(16,), (32,), (64,), (128,), (32, 32,), (64, 64,)]
+    act = ['tanh', 'relu']
+    alpha = [1e-5, 1e-4, 1e-3, 1e-2]
+    max_iter = [10000]
+
+    hyper_parameters_to_test = {
+        'svm': [
+            {'kernel': ['rbf'], 'gamma': gamma, 'C': c_weight, 'class_weight': class_weight},
+            {'kernel': ['linear'], 'C': c_weight, 'class_weight': class_weight},
+            {'kernel': ['poly'], 'degree': deg, 'gamma': gamma, 'C': c_weight, 'class_weight': class_weight}
+        ],
+        'log-reg': [{'penalty': ['l1', 'l2'], 'C': c_weight, 'class_weight': class_weight}],
+        'RF': [{'n_estimators': [10], 'criterion': ['gini', 'entropy'], 'class_weight': class_weight}],
+        'NN': [
+            {
+                'hidden_layer_sizes': hu, 'activation': act, 'alpha': alpha, 'max_iter': max_iter,
+                'solver': ['sgd'], 'learning_rate': ['invscaling', 'constant'],
+                'momentum': [.1, .9], 'nesterovs_momentum': [True],
+                'learning_rate_init': lr
+            },
+            {
+                'hidden_layer_sizes': hu, 'activation': act, 'alpha': alpha, 'max_iter': max_iter,
+                'solver': ['adam'], 'learning_rate_init': lr
+            }
+        ]
+    }
+
+    svm_trials = (len(gamma) * len(c_weight) * len(class_weight)) * (len(deg) + 1) + len(c_weight) * len(class_weight)
+    log_trials = len(c_weight) * len(class_weight) * 2
+    rf_trials = len(class_weight) * 2
+    class_trials = svm_trials + log_trials + rf_trials
+
+    """
+            CREATING DATA SET
+    
+    """
+    data_cols = [
+        'n_fix',
+        # 'fix_max', 'fix_mean',
+        'norm_fix_max', 'norm_fix_mean'
+    ]
+    if saccade:
+        data_cols += [
+            'x_regressions', 'y_regressions',
+            'up_freq', 'down_freq', 'left_freq', 'right_freq',
+            'min_duration', 'avg_duration', 'max_duration',
+            'min_vel', 'avg_vel', 'max_vel',
+            'min_ampl', 'avg_ampl', 'max_ampl',
+            'min_angle', 'avg_angle', 'max_angle',
+            'min_distance', 'avg_distance', 'max_distance',
+            'min_slope', 'avg_slope', 'max_slope'
+        ]
+    grouped_var = {
+        'NR': ['NWoI', 'RWoI'],
+        'N': ['NWoI', 'NWI'],
+        'R': ['RWoI', 'RWI']
+    }
+
+    df = pd.read_csv(data_path)
+
+    if sbj_normalize:
+        for c_var in data_cols:
+            for i in range(len(df)):
+                df[c_var].iloc[i] -= df[df['RECORDING_SESSION_LABEL'] == df['RECORDING_SESSION_LABEL'].iloc[i]][c_var].mean()
+
+    dataset = {}
+    for k, v in grouped_var.items():
+        cdf = df[df[class_column].isin(v)]
+        cdf[class_column] = cdf[class_column].astype('category')
+
+        dataset[k] = cdf.copy()
+
+    total_cases = class_trials * len(normalizers) * len(grouped_var) * len(cdf['minimum_length'].unique()) * trials
+    print(total_cases)
+
+    results = []
+    for ck, cv in classifier_to_test.items():
+        print("\n\n  ---   Classifier: {}".format(ck))
+        for nk, nv in normalizers.items():
+            print("\n\n  ---   Normalizer: {}".format(nk))
+            for k, v in dataset.items():
+                print("\n\n     -----     Data type: {}\n\n".format(k))
+
+                for lv in v['minimum_length'].unique():
+                    print("\n\n     -----     Min Length: {}\n\n".format(lv))
+
+                    curr_len_df = v[v['minimum_length'] == lv]
+                    for t in range(trials):
+                        print("\n        -------        Trial: {}\n".format(t))
+
+                        if test_rate > 0:
+                            tclf, [_, x_test, _, y_test] = grid_search(
+                                data=curr_len_df[data_cols],
+                                labels=curr_len_df[class_column].cat.codes.values,
+                                test_size=test_rate,
+                                multi_class=False,
+                                normalizer_function=nv,
+                                classifier=cv,
+                                hyper_parameters=hyper_parameters_to_test[ck]
+                            )
+                            # acc = compute_confusion(tclf, out_data)
+                            y_pred = tclf.predict(x_test)
+                            results.append([
+                                k, nk, ck, lv,
+                                accuracy_score(y_test, y_pred), f1_score(y_test, y_pred),
+                                str(tclf.best_params_)])
+
+                        else:
+                            new_results_row = [k, nk, ck, lv]
+                            for score in scores:
+                                tclf = grid_search_notest(
+                                    data=curr_len_df[data_cols],
+                                    labels=curr_len_df[class_column].cat.codes.values,
+                                    multi_class=False,
+                                    normalizer_function=nv,
+                                    score=score,
+                                    classifier=cv,
+                                    hyper_parameters=hyper_parameters_to_test[ck]
+                                )
+                                best_score_avg = tclf.cv_results_['mean_test_score'].max()
+                                b_index = tclf.cv_results_['mean_test_score'].argmax()
+                                best_score_std = tclf.cv_results_['std_test_score'][b_index]
+                                new_results_row.append(best_score_avg)
+                                new_results_row.append(best_score_std)
+                                new_results_row.append(str(tclf.best_params_))
+
+                            results.append(new_results_row)
+
+                    results_df = pd.DataFrame(data=results, columns=out_cols)
+                    results_df.to_csv(out_name, index=False)
+
+    if test_rate > 0:
+        g_var = ['Test', 'Normalization', 'Classifier', 'Min_length']
+        s_var = {'Accuracy': 'avg_Acc', 'F1': 'avg_F1'}
+        gr = results_df.groupby(g_var)[list(s_var.keys())].mean().rename(columns=s_var)
+        gr[['std_Acc', 'std_F1']] = results_df.groupby(g_var)[list(s_var.keys())].std()
+
+        gr.reset_index().to_csv('results_stats.csv', index=False)
